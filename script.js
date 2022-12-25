d4698c3fc9744ded86ef3454fbb5a5749296c7a7
Sarah <slowtalon@gmail.com>
2022-12-24 22:09:55 -0800

    set timing for all animations of icons entering

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const timeline = gsap.timeline({ defaults: { duration: 1 } });
 timeline
-  //   .from(".intro", {
-  //     duration: 2,
-  //     ease: "elastic.out(.4, 0.3)",
-  //     x: "-100vw",
-  //   })
   .to(
     ".intro",
     {
@@ -12,34 +7,20 @@ timeline
     },
     2
   )
-  .to(".experience", { opacity: 1 })
   .from(
-    ".fa-react",
-    { duration: 2, ease: "power2.in", opacity: 1, x: "100vw" },
-    4
+    ".experience",
+    {
+      duration: 1.5,
+      ease: "bounce.out",
+      opacity: 1,
+      y: "-100vh",
+      strength: 0.1,
+      squash: 2,
+    },
+    2.1
   )
   .from(
-    ".fa-php",
-    { duration: 2, ease: "power2.in", opacity: 1, x: "100vw" },
-    4.5
-  )
-  .from(
-    ".fa-square-js",
-    { duration: 2, ease: "power2.in", opacity: 1, x: "100vw" },
-    5
-  )
-  .from(
-    ".fa-wordpress-simple",
-    { duration: 2, ease: "power2.in", opacity: 1, x: "100vw" },
-    5.5
-  )
-  .from(
-    ".fa-html5",
-    { duration: 2, ease: "power2.in", opacity: 1, x: "100vw" },
-    6
-  )
-  .from(
-    ".fa-css3-alt",
-    { duration: 2, ease: "power2.in", opacity: 1, x: "100vw" },
-    6.5
+    ".fa-5x",
+    { duration: 2, ease: "power2.in", x: "100vw", stagger: 0.5 },
+    2.5
   );
