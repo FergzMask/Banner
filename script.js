642e5c37a30834ca2d7f396bad0e447746c444f6
Sarah <slowtalon@gmail.com>
2022-12-25 00:04:53 -0800

    added more text and finished adding icons

--- script.js
+++ script.js
@@ -29,4 +29,13 @@ timeline
     },
     8.5
   )
-  .to(".fa-5x", { duration: 3, y: "100vh" }, 8.5);
+  .from(".sarah", { y: "-100vh" }, 8)
+  .to(".fa-5x", { duration: 3, y: "100vh" }, 8.5)
+  .to(".hiring", { opacity: 1, stagger: 0.9 }, 9.6)
+  .to(".lto", { opacity: 0 }, 12)
+  .to(".lto", { opacity: 1 }, 12.5)
+  .to(".lto", { opacity: 0 }, 13)
+  .to(".lto", { opacity: 1 }, 13.5)
+  .to(".lto", { opacity: 0 }, 14)
+  .to(".lto", { opacity: 1 }, 14.5)
+  .to(".hiring", { duration: 3, opacity: 1, x: "100vw" }, 16.5);
